12b2c7a06097d4be8cb5771fda7bb35a5e2aa015
PavelWrki5W10 <fire_ra@bk.ru>
2021-01-09 22:01:44 +0300

    Lesson 2.13

--- app/src/main/java/com/vk/directop/aboutmeapp/MainActivity.kt
+++ app/src/main/java/com/vk/directop/aboutmeapp/MainActivity.kt
@@ -7,5 +7,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //star_position(22.0)
     }
+    /*
+    fun star_position(imt: Double){
+
+        if (imt < 16)
+        if (imt > 16 && imt < 18.5)
+        if (imt > 18.5 && imt < 25)
+        if (imt > 25 && imt < 30)
+        if (imt > 30 && imt < 35)
+        if (imt > 35 && imt < 40)
+        if (imt > 40)
+    }*/
 }
