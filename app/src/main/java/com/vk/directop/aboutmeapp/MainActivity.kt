332728064236bac754cbc9609447ad205a9c10c5
Pavel MSI GL62 <fire_ra@bk.ru>
2021-01-10 19:07:18 +0300

    Now finished 2.19

--- app/src/main/java/com/vk/directop/aboutmeapp/MainActivity.kt
+++ app/src/main/java/com/vk/directop/aboutmeapp/MainActivity.kt
@@ -1,13 +1,68 @@
 package com.vk.directop.aboutmeapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.vk.directop.aboutmeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val myName: MyName = MyName("Ivan Mihalkov")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
+
+        //it - in this context refers to the Done button
+        //which is the view passed as an argument.
+        //findViewById<Button>(R.id.done_button).setOnClickListener {
+        //    addNickname(it)
+        //}
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+
         //star_position(22.0)
+    }
+
+    private fun addNickname(view: View){
+
+
+        binding.apply {
+            //nicknameText.text = binding.nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
+            Log.d("MyTag", myName?.nickname + " is nickname and name is " + myName?.name)
+            //invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.text = myName?.nickname
+            nicknameText.visibility = View.VISIBLE
+        }
+
+        /*
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE  */
+
+
+        //Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
     /*
     fun star_position(imt: Double){
---
+++ app/src/main/java/com/vk/directop/aboutmeapp/MyName.kt
@@ -0,0 +1,6 @@
+package com.vk.directop.aboutmeapp
+
+data class MyName (
+    var name: String = "",
+    var nickname: String = ""
+        )
